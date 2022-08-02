b0dcd91543f3adf2e2cb6a8aa1e4d396b9fca871
HyoungWook Nam <hwnam831@gmail.com>
2022-08-02 12:52:02 -0400

    changed to mydata

--- profile.py
+++ profile.py
@@ -113,15 +113,16 @@ for i in range(params.nodeCount):
 for i, node in enumerate(nodes[1:]):
     node.addService(rspec.Execute(shell="bash", command="/local/repository/start.sh secondary {}.{} {} > /home/cloudlab-openwhisk/start.log 2>&1 &".format(
       BASE_IP, i + 2, params.startKubernetes)))
-    node.addService(rspec.Execute(shell="bash", command="git clone https://github.com/hwnam831/jRAPL-percore ~/jrapl"))
+    node.addService(rspec.Execute(shell="bash", command="sudo mkdir /mydata/workspace; sudo chown hwnam831 /mydata/workspace"))
+    node.addService(rspec.Execute(shell="bash", command="git clone https://github.com/hwnam831/jRAPL-percore /mydata/workspace/jrapl"))
 
 # Start primary node
 nodes[0].addService(rspec.Execute(shell="bash", command="/local/repository/start.sh primary {}.1 {} {} {} {} {} > /home/cloudlab-openwhisk/start.log 2>&1".format(
   BASE_IP, params.nodeCount, params.startKubernetes, params.deployOpenWhisk, params.numInvokers, params.invokerEngine)))
-nodes[0].addService(rspec.Execute(shell="bash", command="git clone https://github.com/spcl/serverless-benchmarks ~/sebs"))
+nodes[0].addService(rspec.Execute(shell="bash", command="git clone https://github.com/spcl/serverless-benchmarks /mydata/workspace/sebs"))
 nodes[0].addService(rspec.Execute(shell="bash", command="python3 ~/sebs/install.py --openwhisk"))
 nodes[0].addService(rspec.Execute(shell="bash", command=". ~/sebs/python-venv/bin/activate; \
-  ~/sebs/sebs.py storage start minio --output-json ~/sebs/config/minio.json;\
-  jq --argfile file1 ~/sebs/config/minio.json '.deployment.openwhisk.storage = $file1 ' ~/sebs/config/example.json > ~/sebs/config/ow.json"))
+  /mydata/workspace/sebs/sebs.py storage start minio --output-json /mydata/workspace/sebs/config/minio.json;\
+  jq --argfile file1 /mydata/workspace/sebs/config/minio.json '.deployment.openwhisk.storage = $file1 ' /mydata/workspace/sebs/config/example.json > /mydata/workspace/sebs/config/ow.json"))
 
 pc.printRequestRSpec()
