b99a9f1a69dd59c34965a1135951ba3c8d8b5b30
HyoungWook Nam <hwnam831@gmail.com>
2022-08-09 10:42:12 -0400

    Update setup-master.sh

--- setup-master.sh
+++ setup-master.sh
@@ -2,6 +2,15 @@
 git clone https://github.com/PrincetonUniversity/faas-profiler /mydata/workspace/faas-profiler
 cd /mydata/workspace/faas-profiler
 bash configure.sh
+cd functions/img-resize
+sudo npm install node-zip jimp --save
+zip -r action.zip ./*
+wsk action create img-resize --kind nodejs:12 action.zip --web raw -i
+cd ../markdown-to-html/
+wsk action create markdown2html markdown2html.py --docker immortalfaas/markdown-to-html --web raw -i
+cd ../ocr-img/
+wsk action create ocr-img handler.js --docker immortalfaas/nodejs-tesseract --web raw -i
+cd ../sentiment-analysis
 #git clone https://github.com/spcl/serverless-benchmarks /mydata/workspace/sebs
 #cd /mydata/workspace/sebs
 #python3 /mydata/workspace/sebs/install.py --openwhisk
