dade46f4a19f94b2f9e473348243d2e64160a936
HyoungWook Nam <hwnam831@gmail.com>
2022-09-19 17:29:15 -0500

    Update setup-all.sh

--- setup-all.sh
+++ setup-all.sh
@@ -5,3 +5,5 @@ git clone https://github.com/hwnam831/jRAPL-percore /mydata/workspace/jrapl
 sudo modprobe msr
 sudo apt install msr-tools
 sudo wrmsr --all 0x1a0 0x4000850089
+git config --global user.email "hwnam831@gmail.com"
+git config --global user.name "Hyoungwook Nam"
