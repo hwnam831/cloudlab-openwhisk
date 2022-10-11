fc97e2d2ac52d0be2e839c7a2f9426d6cc467554
HyoungWook Nam <hwnam831@gmail.com>
2022-10-10 19:12:17 -0500

    Update setup-all.sh

--- setup-all.sh
+++ setup-all.sh
@@ -2,6 +2,7 @@
 sudo mkdir /mydata/workspace
 sudo chown hwnam831 /mydata/workspace
 git clone https://github.com/hwnam831/jRAPL-percore /mydata/workspace/jrapl
+sudo apt install libpfm4-dev cpufrequtils msr-tools
 sudo modprobe msr
 #sudo wrmsr --all 0x1a0 0x4000850089
 git config --global user.email "hwnam831@gmail.com"
