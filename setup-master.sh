71ae1583ebb195a4f14c367470604ccc725c5dff
HyoungWook Nam <hwnam831@gmail.com>
2022-12-22 18:58:53 +0900

    Update setup-master.sh

--- setup-master.sh
+++ setup-master.sh
@@ -2,9 +2,9 @@
 wsk property set --apihost localhost:31001
 wsk property set --auth 23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
 sudo apt install ansible
-#git clone https://github.com/hwnam831/openwhisk-mlcontrol /mydata/workspace/openwhisk
-#cd /mydata/workspace/openwhisk
-#sudo ./gradlew distDocker
+git clone https://github.com/apache/openwhisk-runtime-python /mydata/workspace/openwhisk-runtime-python
+cd /mydata/workspace/openwhisk-runtime-python
+./gradlew core:python3Action:distDocker
 git clone https://github.com/PrincetonUniversity/faas-profiler /mydata/workspace/faas-profiler
 cd /mydata/workspace/faas-profiler
 git reset --hard b6f64dbfa9583e29c5679c1c9859b3818dc21638
@@ -22,10 +22,6 @@ cd ../ocr-img/
 wsk action create ocr-img handler.js --docker immortalfaas/nodejs-tesseract --web raw -i
 cd ../sentiment-analysis
 wsk action create sentiment sentiment.py --docker immortalfaas/sentiment --web raw -i
-cd ../autocomplete
-sed -i 's/nodejs:6/nodejs:14/g' lib/whisk.js
-sudo npm link
-acsetup data/names.txt
 #git clone https://github.com/spcl/serverless-benchmarks /mydata/workspace/sebs
 #cd /mydata/workspace/sebs
 #python3 /mydata/workspace/sebs/install.py --openwhisk
