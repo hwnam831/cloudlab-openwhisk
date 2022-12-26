fd7d355cc57877c08f84bfccac4c7234e5a89c40
HyoungWook Nam <hwnam831@gmail.com>
2022-12-26 22:10:14 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 wsk action create matmul matmul/matmul.py --docker openwhisk/python3aiaction -i
 wsk action create linpack linpack/linpack.py --docker openwhisk/python3aiaction -i
-docker run --rm -v "$PWD/ml_training:/tmp" openwhisk/python3aiaction bash -c "cd /tmp && virtualenv virtualenv && source virtualenv/bin/activate && pip install --no-input -r requirements.txt"
-cd ml_training
-zip -r ml_training.zip virtualenv __main__.py
-wsk action create ml_training ml_training.zip -m 2048 --docker openwhisk/python3aiaction -i
+wsk action create ml_training ml_training/__main__.py -m 2048 --docker hwnam831/python3mlhrc -i
 cd ..
 
