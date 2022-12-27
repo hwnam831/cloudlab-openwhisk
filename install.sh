15d10612826d4c16667f6bacd3f7520612c886f2
HyoungWook Nam <hwnam831@gmail.com>
2022-12-27 20:39:20 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 wsk action create matmul matmul/matmul.py --docker openwhisk/python3aiaction -i
 wsk action create linpack linpack/linpack.py --docker openwhisk/python3aiaction -i
 wsk action create ml_training ml_training/__main__.py -m 2048 --docker hwnam831/python3mlhrc -i
-cd ..
 
