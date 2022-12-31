b5989c6f08d842c3baaf6303d82f1b897b733b38
HyoungWook Nam <hwnam831@gmail.com>
2022-12-31 14:21:14 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 wsk action create matmul matmul/matmul.py -m 512 --docker hwnam831/actionloop-python-v3.6-ai:latest -i
-wsk action create linpack linpack/linpack.py --docker hwnam831/actionloop-python-v3.6-ai:latest -i
+wsk action create linpack linpack/linpack.py -m 512 --docker hwnam831/actionloop-python-v3.6-ai:latest -i
 wsk action create ml_training ml_training/__main__.py -m 512 --docker hwnam831/actionloop-python-v3.6-ai:latest -i
 
