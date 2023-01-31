b5b7446cf2bd8703487b1790aacf382bec485277
HyoungWook Nam <hwnam831@gmail.com>
2023-01-31 16:11:14 -0600

    Update setup-master.sh

--- setup-master.sh
+++ setup-master.sh
@@ -12,12 +12,12 @@ git clone https://github.com/ddps-lab/serverless-faas-workbench
 #sudo ./minio server /mydata/workspace/miniostorage/ &
 wget https://dl.min.io/server/minio/release/linux-amd64/minio_20230131022419.0.0_amd64.deb
 sudo dpkg -i minio_20230131022419.0.0_amd64.deb
-#MINIO_ROOT_USER=minioadmin MINIO_ROOT_PASSWORD=minioadmin minio server /mydata/workspace/miniostorage --console-address ":9001" &
-sudo groupadd -r minio-user
-sudo useradd -M -r -g minio-user minio-user
-sudo chown minio-user:minio-user /mydata/workspace/miniostorage
-sudo cp /local/repository/miniodefault /etc/default/minio
-sudo systemctl start minio
+MINIO_ROOT_USER=minioadmin MINIO_ROOT_PASSWORD=minioadmin minio server /mydata/workspace/miniostorage --console-address ":9001" &
+#sudo groupadd -r minio-user
+#sudo useradd -M -r -g minio-user minio-user
+#sudo chown minio-user:minio-user /mydata/workspace/miniostorage
+#sudo cp /local/repository/miniodefault /etc/default/minio
+#sudo systemctl start minio
 wget https://dl.minio.io/client/mc/release/linux-amd64/mc
 chmod +x mc
 ./mc alias set myminio/ http://10.10.1.1:9000 minioadmin minioadmin
@@ -57,4 +57,5 @@ cd ../..
 #jq --argfile file1 config/minio.json '.deployment.openwhisk.storage = $file1 ' config/example.json > config/ow.json
 #echo "./sebs.py experiment invoke perf-cost --config config/ow.json --deployment openwhisk --verbose" > perf-cost.sh
 #Check if minio works
-sudo systemctl status minio
+#sudo systemctl status minio
+wsk action invoke -i video_processing --result
