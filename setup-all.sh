3ed1114a1b67ceb10e1d11ea2b93b5ce8a5c266a
HyoungWook Nam <hwnam831@gmail.com>
2023-02-10 14:02:04 -0600

    Update setup-all.sh

--- setup-all.sh
+++ setup-all.sh
@@ -10,3 +10,6 @@ sudo modprobe msr
 #sudo wrmsr --all 0x1a0 0x4000850089
 git config --global user.email "hwnam831@gmail.com"
 git config --global user.name "Hyoungwook Nam"
+docker pull hwnam831/mxcontainer:latest
+docker pull hwnam831/invoker:v2
+docker pull hwnam831/controller:v2
