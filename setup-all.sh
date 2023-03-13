53baa738d1f566a3b3e559b5e2427fcc5e5a1fe1
HyoungWook Nam <hwnam831@gmail.com>
2023-03-13 18:16:55 -0500

    Update setup-all.sh

--- setup-all.sh
+++ setup-all.sh
@@ -2,6 +2,9 @@
 sudo mkdir /mydata/workspace
 sudo chown hwnam831 /mydata/workspace
 git clone https://github.com/hwnam831/jRAPL-percore /mydata/workspace/jrapl
+cd /mydata/workspace
+wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
+unzip libtorch-shared-with-deps-latest.zip
 cd /mydata/workspace/jrapl
 make
 make install
