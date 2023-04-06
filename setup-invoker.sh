46464390c3f201da74681264fc9f67503411b01b
HyoungWook Nam <hwnam831@gmail.com>
2023-04-06 15:31:04 -0500

    Update setup-invoker.sh

--- setup-invoker.sh
+++ setup-invoker.sh
@@ -2,6 +2,10 @@
 cd /local/repository
 bash setup-all.sh
 python3 owPackageController.py &
+git clone https://github.com/hwnam831/jRAPL-percore /mydata/workspace/jrapl
 cd /mydata/workspace
 wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
 unzip libtorch-shared-with-deps-latest.zip
+cd /mydata/workspace/jrapl
+make
+make install
