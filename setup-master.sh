a7684fd367d8f4366d43c8dd1252b204edc55564
Hyoungwook Nam <hwnam831@gmail.com>
2023-04-10 15:47:17 -0500

    new double configs

--- setup-master.sh
+++ setup-master.sh
@@ -34,6 +34,7 @@ cd /mydata/workspace/faas-profiler
 git reset --hard b6f64dbfa9583e29c5679c1c9859b3818dc21638
 bash configure.sh
 cp /local/repository/*.json ./
+cp /local/repository/mxcontainerconfigs/*.json ./
 wsk action create primes-python functions/microbenchmarks/primes/primes-python.py --docker hwnam831/mxcontainer:latest -i
 wsk action create base64-python functions/microbenchmarks/base64/base64-python.py --docker hwnam831/mxcontainer:latest -i
 cd functions/img-resize
