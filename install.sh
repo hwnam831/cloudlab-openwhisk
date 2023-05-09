139fd83a8cf67bfa051bf90276c5c419627ab457
Hyoungwook Nam <hwnam831@gmail.com>
2023-05-09 17:20:22 -0500

    typo

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ wsk action create -i image_rotate image_rotate/lambda.py --docker hwnam831/mxcon
 wsk action create -i lr_serving lr_serving/lambda.py --docker hwnam831/mxcontainer:latest -m 2048
 wsk action create -i video_processing video_processing/lambda.py --docker hwnam831/mxcontainer:latest -m 2048
 wsk action create -i rnn_serving rnn_serving/lambda.py --docker hwnam831/mxcontainer:latest -m 2048
-wsk action create -i sentiment sentiment-anlysis/sentiment.py --docker hwnam831/mxcontainer:latest -m 2048
+wsk action create -i sentiment sentiment-analysis/sentiment.py --docker hwnam831/mxcontainer:latest -m 2048
 wsk action create -i markdown2html markdown-to-html/markdown2html.py --docker hwnam831/mxcontainer:latest -m 2048
 
 wsk action create -i matmul1 matmul/matmul.py --docker hwnam831/mxcontainer:latest -m 2048
@@ -17,5 +17,5 @@ wsk action create -i image_rotate1 image_rotate/lambda.py --docker hwnam831/mxco
 wsk action create -i lr_serving1 lr_serving/lambda.py --docker hwnam831/mxcontainer:latest -m 2048
 wsk action create -i video_processing1 video_processing/lambda.py --docker hwnam831/mxcontainer:latest -m 2048
 wsk action create -i rnn_serving1 rnn_serving/lambda.py --docker hwnam831/mxcontainer:latest -m 2048
-wsk action create -i sentiment1 sentiment-anlysis/sentiment.py --docker hwnam831/mxcontainer:latest -m 2048
+wsk action create -i sentiment1 sentiment-analysis/sentiment.py --docker hwnam831/mxcontainer:latest -m 2048
 wsk action create -i markdown2html1 markdown-to-html/markdown2html.py --docker hwnam831/mxcontainer:latest -m 2048
