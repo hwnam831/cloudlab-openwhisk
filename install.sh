6e524f1ef4fce9aa56d91c74ed7089fbdb07a32b
Hyoungwook Nam <hwnam831@gmail.com>
2023-09-26 16:58:19 -0500

    two node config

--- install.sh
+++ install.sh
@@ -15,3 +15,21 @@ wsk action create -i image_rotate_socket1 image_rotate/lambda.py --docker hwnam8
 wsk action create -i lr_serving_socket1 lr_serving/lambda.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 512
 wsk action create -i video_processing_socket1 video_processing/lambda.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 512
 wsk action create -i rnn_serving_socket1 rnn_serving/lambda.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 512
+
+wsk action create -i matmul_node1 matmul/matmul.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 512
+wsk action create -i linpack_node1 linpack/linpack.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 512
+wsk action create -i ml_training_node1 ml_training/__main__.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 512
+wsk action create -i cnn_serving_node1 cnn_serving/lambda.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 512
+wsk action create -i image_rotate_node1 image_rotate/lambda.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 1024
+wsk action create -i lr_serving_node1 lr_serving/lambda.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 512
+wsk action create -i video_processing_node1 video_processing/lambda.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 512
+wsk action create -i rnn_serving_node1 rnn_serving/lambda.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 512
+
+wsk action create -i matmul_node1_socket1 matmul/matmul.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 512
+wsk action create -i linpack_node1_socket1 linpack/linpack.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 512
+wsk action create -i ml_training_node1_socket1 ml_training/__main__.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 512
+wsk action create -i cnn_serving_node1_socket1 cnn_serving/lambda.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 512
+wsk action create -i image_rotate_node1_socket1 image_rotate/lambda.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 1024
+wsk action create -i lr_serving_node1_socket1 lr_serving/lambda.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 512
+wsk action create -i video_processing_node1_socket1 video_processing/lambda.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 512
+wsk action create -i rnn_serving_node1_socket1 rnn_serving/lambda.py --docker hwnam831/actionloop-python-v3.6-ai:latest -m 512
--- install_functions.sh
+++ install_functions.sh
@@ -10,20 +10,32 @@ wsk action create primes-python functions/microbenchmarks/primes/primes-python.p
 wsk action create base64-python functions/microbenchmarks/base64/base64-python.py --docker hwnam831/actionloop-python-v3.6-ai:latest -i
 wsk action create primes-python_socket1 functions/microbenchmarks/primes/primes-python.py --docker hwnam831/actionloop-python-v3.6-ai:latest -i
 wsk action create base64-python_socket1 functions/microbenchmarks/base64/base64-python.py --docker hwnam831/actionloop-python-v3.6-ai:latest -i
+wsk action create primes-python_node1 functions/microbenchmarks/primes/primes-python.py --docker hwnam831/actionloop-python-v3.6-ai:latest -i
+wsk action create base64-python_node1 functions/microbenchmarks/base64/base64-python.py --docker hwnam831/actionloop-python-v3.6-ai:latest -i
+wsk action create primes-python_node1_socket1 functions/microbenchmarks/primes/primes-python.py --docker hwnam831/actionloop-python-v3.6-ai:latest -i
+wsk action create base64-python_node1_socket1 functions/microbenchmarks/base64/base64-python.py --docker hwnam831/actionloop-python-v3.6-ai:latest -i
 cd functions/img-resize
 sudo npm install node-zip jimp --save
 zip -r action.zip ./*
 wsk action create img-resize --kind nodejs:14 action.zip --web raw -i -m 512
 wsk action create img-resize_socket1 --kind nodejs:14 action.zip --web raw -i -m 512
+wsk action create img-resize_node1 --kind nodejs:14 action.zip --web raw -i -m 512
+wsk action create img-resize_node1_socket1 --kind nodejs:14 action.zip --web raw -i -m 512
 cd ../markdown-to-html/
 wsk action create markdown2html markdown2html.py --docker immortalfaas/markdown-to-html --web raw -i
 wsk action create markdown2html_socket1 markdown2html.py --docker immortalfaas/markdown-to-html --web raw -i
+wsk action create markdown2html_node1 markdown2html.py --docker immortalfaas/markdown-to-html --web raw -i
+wsk action create markdown2html_node1_socket1 markdown2html.py --docker immortalfaas/markdown-to-html --web raw -i
 cd ../ocr-img/
 wsk action create ocr-img handler.js --docker immortalfaas/nodejs-tesseract --web raw -i -m 512
 wsk action create ocr-img_socket1 handler.js --docker immortalfaas/nodejs-tesseract --web raw -i -m 512
+wsk action create ocr-img_node1 handler.js --docker immortalfaas/nodejs-tesseract --web raw -i -m 512
+wsk action create ocr-img_node1_socket1 handler.js --docker immortalfaas/nodejs-tesseract --web raw -i -m 512
 cd ../sentiment-analysis
 wsk action create sentiment sentiment.py --docker immortalfaas/sentiment --web raw -i
 wsk action create sentiment_socket1 sentiment.py --docker immortalfaas/sentiment --web raw -i
+wsk action create sentiment_node1 sentiment.py --docker immortalfaas/sentiment --web raw -i
+wsk action create sentiment_node1_socket1 sentiment.py --docker immortalfaas/sentiment --web raw -i
 cd ../..
 
 wsk action invoke -i video_processing --result
