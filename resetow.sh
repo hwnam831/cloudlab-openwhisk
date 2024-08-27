50f640c07ed610758f838a28c5a1a6ad3494db1c
Hyoungwook Nam <hwnam831@gmail.com>
2024-08-27 13:43:22 -0500

    cleanup unnecessary files

--- resetow.sh
+++
@@ -1,4 +0,0 @@
-cd /home/cloudlab-openwhisk/openwhisk-deploy-kube
-sudo helm uninstall owdev -n openwhisk
-kubectl delete namespace openwhisk; kubectl create namespace openwhisk
-sudo helm install owdev ./helm/openwhisk -n openwhisk -f mycluster.yaml
