edce9c5320d1f8fde4019e8ddf3cb33309e66034
Hyoungwook Nam <hwnam831@gmail.com>
2025-02-03 16:19:14 -0600

    leave one core for the controller

--- pull-containers.sh
+++ pull-containers.sh
@@ -1,4 +1,4 @@
-sudo docker pull hwnam831/gnn-training
-sudo docker pull hwnam831/stable-diffusion
-sudo  docker pull hwnam831/vits-ljs
+sudo docker pull hwnam831/gnn-training &
+sudo docker pull hwnam831/stable-diffusion &
+sudo  docker pull hwnam831/vits-ljs &
 sudo docker pull hwnam831/llama-3.1-8b
