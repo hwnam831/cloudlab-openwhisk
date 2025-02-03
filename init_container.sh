5bff6dfa34a3a8ec11a5e22ab23d40f91292d2bc
Hyoungwook Nam <hwnam831@gmail.com>
2025-02-03 17:11:57 -0600

    Roll back to full core usage

--- init_container.sh
+++ init_container.sh
@@ -2,6 +2,6 @@
 #usage: bash init_containers.sh [containername] [socket]
 NCPUS=$(nproc)
 SOCKETCPUS=$((NCPUS/2))
-OFFSET=$(($2 * $SOCKETCPUS + 1))
-CPUSET="$OFFSET-$(($OFFSET + $SOCKETCPUS - 2))"
+OFFSET=$(($2 * $SOCKETCPUS))
+CPUSET="$OFFSET-$(($OFFSET + $SOCKETCPUS - 1))"
 sudo docker run -d -it --cpuset-cpus=$CPUSET --name="$1_$2" hwnam831/$1
