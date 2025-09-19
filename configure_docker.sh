3318367a2ddb1941a120170d5fb1d44027347169
HyoungWook Nam <hwnam831@gmail.com>
2025-09-19 11:33:59 -0700

    Update configure_docker.sh

--- configure_docker.sh
+++ configure_docker.sh
@@ -1,11 +1,12 @@
 sudo mkdir -p /mydata/docker
 sudo mkdir -p /mydata/workspace
+sudo chmod 777 /mydata/workspace
 sudo modprobe msr
 for FILE in /users/*; do
     CURRENT_USER=${FILE##*/}
-    usermod -aG docker $CURRENT_USER
+    sudo usermod -aG docker $CURRENT_USER
 done
-
+newgrp docker
 echo -e '{
     "exec-opts": ["native.cgroupdriver=systemd"],
     "log-driver": "json-file",
@@ -17,3 +18,9 @@ echo -e '{
 }' | sudo tee /etc/docker/daemon.json
 sudo systemctl restart docker || (echo "ERROR: Docker installation failed, exiting." && exit -1)
 sudo docker run hello-world | grep "Hello from Docker!" || (echo "ERROR: Docker installation failed, exiting." && exit -1)
+sudo docker pull hwnam831/torchbench
+
+git clone https://github.com/hwnam831/jRAPL-percore /mydata/workspace/jrapl
+cd /mydata/workspace/jrapl
+make
+make install
